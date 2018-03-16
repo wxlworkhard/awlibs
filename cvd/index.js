c22945d75a071a080aa40b5da7332320aa21ddc3
wangxiaolong11 <wangxiaolong11@meituan.com>
2018-03-16 17:32:23 +0800

    ADD: cvd 分层模式

---
+++ cvd/index.js
@@ -0,0 +1,148 @@
+import formhandler from '../formhandler';
+import Lazychain from '../lazychain';
+
+
+function CVD(store) {
+    this.store = store;
+}
+
+CVD.dispatch = formhandler.dispatch;
+CVD.register = formhandler.register;
+
+CVD.prototype.getStore = function () {
+    return {
+        ...this.store,
+    };
+}
+
+CVD.prototype.setStore = function (store) {
+    return this.store = {
+        ...this.store,
+        ...store,
+    };
+}
+
+CVD.prototype.init = function (connectorHandle, validatorHandle, dependencyHandle) {
+    this.initConnector(connectorHandle);
+    this.initValidator(validatorHandle);
+    this.initDependency(dependencyHandle);
+};
+
+CVD.prototype.flow = function (params) {
+    const lc = new Lazychain(params);
+
+    lc.tap((cvdResult) => {
+        return this.connector({
+            ...cvdResult
+        });
+    }).tap((cvdResult) => {
+        return this.validator({
+            ...cvdResult,
+        });
+    }).tap((cvResult) => {
+        return this.dependency({
+            ...cvResult,
+        });
+    });
+
+    lc.force();
+
+    return this.getStore();
+}
+
+
+
+CVD.prototype.initConnector = function (connectorHandle) {
+    this.connector = (function (args) {
+        let { key, value, params } = args;
+        let { model } = this.store;
+
+        let cRet = connectorHandle(key, value, params, this.store);
+        cRet = cRet == null ? value : cRet;
+
+        if (Object.keys(model).indexOf(key) != -1) {
+            model = {
+                ...model,
+                [key]: cRet == null ? getDefaultValue(model, key) : cRet
+            };
+        }
+
+        this.store = {
+            ...this.store,
+            model,
+        };
+
+        return {
+            ...args,
+            cRet,
+        };
+    }).bind(this);
+};
+
+CVD.prototype.initValidator = function (validatorHandle) {
+    this.validator = (function (args) {
+        /**
+         * key 校验的控件标准   Required
+         * cRet 要校验的值
+         */
+        let { key, cRet, params } = args;
+        let { desc } = this.store;
+
+        let vRet = {
+            valid: true,
+            msg: ''
+        };
+
+        vRet = validatorHandle(key, cRet, params) || vRet;
+
+        desc = {
+            ...desc,
+            [key]: {
+                ...desc[key],
+                ...vRet
+            }
+        };
+
+        this.store.desc = desc;
+
+        return {
+            ...args,
+            vRet,
+        };
+    }).bind(this);
+}
+
+CVD.prototype.initDependency = function (dependencyHandle) {
+    this.dependency = (function (args) {
+        let { key, cRet, params } = args;
+        let { desc } = this.store;
+
+        let store = dependencyHandle(key, cRet, params, this.store);
+
+        this.store = {
+            ...this.store,
+            ...store
+        };
+        return {
+            ...args,
+        };
+    }).bind(this);
+}
+
+function getDefaultValue(obj, key) {
+    const type = Object.prototype.toString.call(obj[key]);
+    const typeValueMap = {
+        '[object String]': '',
+        '[object Object]': {},
+        '[object Array]': [],
+    };
+
+    let ret = typeValueMap[type];
+
+    if (ret == null) {
+        ret = '';
+    }
+    return ret;
+}
+
+module.exports = CVD;
